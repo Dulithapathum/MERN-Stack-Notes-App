5880615a2699ce684a6ba6f8a0f72c48f2945480
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2024-10-28 11:20:18 +0530

    React Router Dom Config

--- Frontend/notes-app/src/App.jsx
+++ Frontend/notes-app/src/App.jsx
@@ -1,12 +1,20 @@
-import'./App.css'
-import Home from './pages/Home/Home'
-
+import "./App.css";
+import Home from "./pages/Home/Home";
+import LogIn from "./pages/LogIn/LogIn";
+import SignUp from "./pages/SignUP/SignUP";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 const App = () => {
   return (
     <div>
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="dashboard" element={<Home />}></Route>
+          <Route path="login" element={<LogIn />}></Route>
+          <Route path="signup" element={<SignUp />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
