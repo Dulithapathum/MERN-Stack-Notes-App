a6ce940e62f050d805f96975a0a13b54d94c0b05
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2024-11-02 08:22:58 +0530

    Login Page UI Design

---
+++ Frontend/notes-app/src/components/Input/PasswordInput.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
+const PasswordInput = ({ value, onChange, placeholder }) => {
+  const [isShowpassword, setIsShowPassword] = useState(false);
+
+  const toggleShowPassword = () => {
+    setIsShowPassword(!isShowpassword);
+  };
+  return (
+    <div className="flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3">
+      <input
+        type={isShowpassword ? "text" : "password"}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder || "password"}
+        className="w-full text-sm bg-transparent py-3 mr-3 rounded outline-none"
+      />
+      {isShowpassword ? (
+        <FaRegEye
+          size={22}
+          className="text-primary cursor-pointer"
+          onClick={() => {
+            toggleShowPassword();
+          }}
+        />
+      ) : (
+        <FaRegEyeSlash
+          size={22}
+          className="text-slate-400 cursor-pointer"
+          onClick={() => {
+            toggleShowPassword();
+          }}
+        />
+      )}
+    </div>
+  );
+};
+
+export default PasswordInput;
--- Frontend/notes-app/src/pages/LogIn/LogIn.jsx
+++ Frontend/notes-app/src/pages/LogIn/LogIn.jsx
@@ -1,9 +1,68 @@
-
-
+import { useState } from "react";
+import PasswordInput from "../../components/Input/PasswordInput";
+import NavBar from "../../components/NavBar/NavBar";
+import { Link } from "react-router-dom";
+import validateEmail from "../../utils/helper";
 const LogIn = () => {
-  return (
-    <div>LogIn</div>
-  )
+  const [email, setEmail] = useState("");
+  const [password, setPassowrd] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleLogIn = async (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError("Please Enter Valid Email Adderess.");
+      return;
+    }
+
+if(!password){
+  setError("Please Enter The Password")
+  return
 }
 
-export default LogIn
+    setError("")
+
+    // Login Api Call
+  };
+
+  return (
+    <div>
+      <NavBar />
+      <div className=" flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form onSubmit={handleLogIn}>
+            <h4 className="text-2xl mb-7 text-center">LogIn</h4>
+            <input
+              type="text"
+              placeholder="Email"
+              className="input-box"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <PasswordInput
+              value={password}
+              onChange={(e) => {
+                setPassowrd(e.target.value);
+              }}
+            />
+            {error && <p className="text-red-500 text-sm pb-1">{error}</p>}
+            <button type="submit" className="btn-primary">
+              LogIn
+            </button>
+            <p className="text-sm text-center mt-4">
+              Not Rejister Yet?
+              <Link to="/signup" className="font-medium text-primary underline">
+                Create An Account
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LogIn;
