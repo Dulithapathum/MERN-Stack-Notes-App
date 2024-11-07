24ec42d34c9f9560621257b05f05cf4934d7ba4f
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2024-11-07 20:42:13 +0530

    ENV File And jwt authentication funtion created

---
+++ Backend/utilities.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const  dotenv=require('dotenv')
+dotenv.config();
+
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (!token) return res.status(401).json({ error: "Token missing or invalid" });
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.status(403).json({ error: "Token verification failed" }); 
+
+        req.user = user;
+        next();
+    });
+}
+
+module.exports = { authenticateToken };
