2152a89bf35e755d2cc840676f1d950fd0ff3fce
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2024-11-07 21:05:18 +0530

    Connect MongoDB

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
+
+dotenv.config(); 
+
+const app = express();
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.error("Failed to connect to MongoDB", err));
+
+
 app.get("/", (req, res) => { 
-  res.json({ data: "hello" });
+    res.json({ data: "hello" });
 });
 
+
+
 app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+    console.log("Server is running on port 8000");
 });
 
 module.exports = app;
