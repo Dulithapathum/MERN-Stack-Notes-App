6b5b3bdaa6b286d563cc8496af41af50647580df
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2024-11-08 11:21:48 +0530

    Create Account  API Created

--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,76 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 
+dotenv.config();
 
-dotenv.config(); 
+const User = require("./models/userModel");
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Failed to connect to MongoDB", err));
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
-.then(() => console.log("Connected to MongoDB"))
-.catch((err) => console.error("Failed to connect to MongoDB", err));
-
-
-app.get("/", (req, res) => { 
-    res.json({ data: "hello" });
+app.get("/", (req, res) => {
+  res.json({ data: "hello" });
 });
 
+app.post("/create-account", async (req, res) => {
+  const { fullName, email, password } = req.body;
 
+  if (!fullName) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Full Name Is Required" });
+  }
+
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Email Is Required" });
+  }
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password Is Required" });
+  }
+
+  const isUser = await User.findOne({ email: email });
+
+  if (isUser) {
+    return res.json({
+      error: true,
+      message: "user already exist",
+    });
+  }
+
+  const user = new User({
+    fullName,
+    email,
+    password,
+  });
+
+  await user.save();
+
+  const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "36000m",
+  });
+
+  return res.json({
+    error: false,
+    user,
+    accessToken,
+    message: "Rejister Successful",
+  });
+});
 
 app.listen(8000, () => {
-    console.log("Server is running on port 8000");
+  console.log("Server is running on port 8000");
 });
 
 module.exports = app;
